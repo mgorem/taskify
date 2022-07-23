5a282819595bf4d406df4b692b382c9c6d47cc7a
mgorem <oremgitonga22@gmail.com>
2022-07-23 22:29:59 +0300

    state for todo working

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { SetStateAction } from 'react';
+import './styles.css';
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
-    <div>
-      InputField
-    </div>
+    <form className='input'>
+      <input type='input' 
+      placeholder='Enter a task' 
+      value={todo} 
+      onChange={(e => setTodo(e.target.value))} 
+      className='input__box' 
+      />
+      <button type='submit' className='input__submit'>GO</button>
+    </form>
   )
 }
 
